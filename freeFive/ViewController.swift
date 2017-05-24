c22267306fca668a86e5a5a83a79a3f5e314a131
John Baker <john@b4k3r.com>
2017-05-23 23:00:04 -0400

    Initial Commit

---
+++ freeFive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  freeFive
+//
+//  Created by John Baker on 5/23/17.
+//  Copyright © 2017 B4k3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
